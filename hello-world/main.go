a578b43f2be2745ee50534ff94379b04ca38be04
yosnak13 <yosnak13@gmail.com>
2024-01-25 17:32:27 +0900

    EventBridgeルール追記

--- hello-world/main.go
+++ hello-world/main.go
@@ -3,29 +3,11 @@ package main
 import (
 	"fmt"
 
-	"github.com/aws/aws-lambda-go/events"
 	"github.com/aws/aws-lambda-go/lambda"
 )
 
-func handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
-	var greeting string
-	sourceIP := request.RequestContext.Identity.SourceIP
-
-	if sourceIP == "" {
-		greeting = "Hello, world!\n"
-	} else {
-		greeting = fmt.Sprintf("Hello, %s!\n", sourceIP)
-	}
-
-	return events.APIGatewayProxyResponse{
-		Body:       greeting,
-		StatusCode: 200,
-	}, nil
-}
-
-func invokeFromEventBridge(event events.CloudWatchEvent) (string, error) {
-	fmt.Println("Received event:", event)
-	return "Hello, World!", nil
+func handler() {
+	fmt.Println("Hello World")
 }
 
 func main() {
